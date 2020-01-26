e90f27e5bf7d8a9f55b807d8ab498cb1d8f90324
alejandro-colomar <colomar.6.4.3@gmail.com>
2020-01-26 01:42:44 +0100

    Remove irrelevant old comment

--- ucat.c
+++ ucat.c
@@ -2,22 +2,6 @@
  *	Copyright (C) 2020	Alejandro Colomar Andrés		      *
  *	SPDX-License-Identifier:	BSD-2-Clause			      *
  ******************************************************************************/
-/*
- * This example source code uses a C library buffered I/O interface to
- * URL reads.  It supports fopen(), fread(), fgets(), feof(), fclose(),
- * rewind(). Supported functions have identical prototypes to their normal
- * libc namesakes and are preceaded by url_ .
- *
- * Using this code you can replace your program's fopen() with url_fopen()
- * and fread() with url_fread() and it becomes possible to read remote streams
- * instead of (only) local files. Local files (ie those that can be directly
- * fopened) will drop back to using the underlying libc implementations.
- *
- * See the main() function that shows an app that retrieves from a specified
- * url using url_fread() and writes to stdout (similar to cat).
- *
- * This code requires libcurl 7.17 or later and libalx 1~b10 or later.
- */
 
 #include <stdbool.h>
 #include <stdio.h>
